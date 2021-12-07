79e3658f3d1b1d85933679a01c7ea741793e6b3b
HumaAltaf5 <huma.altaf@gmail.com>
2021-12-07 10:49:21 +0000

    Implementing Exercise002 - HumaAltaf5

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -2,14 +2,34 @@ package com.techreturners.exercise002;
 
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
-        // Add your code here
-        return false;
+    public boolean isFromManchester(Person person) {
+        if (person.city == "Manchester") {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
-        // Add your code here
-        return false;
+    public boolean canWatchFilm(Person person, int ageLimit) {
+        if (person.age >= ageLimit) {
+            return true;
+        } else {
+            return false;
+        }
     }
     
+}
+
+class Person {
+    String name;
+    String surname;
+    String city;
+    int age;
+
+    public Person(String personName, String personSurname, String personCity, int personAge) {
+        name =  personName;
+        surname = personSurname;
+        city = personCity;
+        age = personAge;
+    }
 }
