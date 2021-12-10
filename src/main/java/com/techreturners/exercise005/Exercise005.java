ce0c62052642d0dff88c613524dec4ca6509c44e
HumaAltaf5 <huma.altaf@gmail.com>
2021-12-10 10:08:56 +0000

    Implementing Exercise005.java - HumaAltaf

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -3,7 +3,17 @@ package com.techreturners.exercise005;
 public class Exercise005 {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        if (input == null) {
+            throw new NullPointerException("Cannot check for Pangram for null String");
+        } else {
+            //Eliminate spaces
+            input = input.toLowerCase().replaceAll("\\s+", "");
+            //Eliminate all non alphabetic characters
+            input = input.replaceAll("[^A-Za-z]", "");
+            //Eliminate Duplicate alphabetic characters
+            input = input.replaceAll("(.)(?=.*\\1)", "");
+            // check for complete alphabet
+            return (input.length() == 26);
+        }
     }
-
 }
--- src/test/java/com/techreturners/exercise005/Exercise005Test.java
+++ src/test/java/com/techreturners/exercise005/Exercise005Test.java
@@ -4,8 +4,8 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class Exercise005Test {
 
@@ -21,52 +21,91 @@ public class Exercise005Test {
         assertFalse(ex005.isPangram(""));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkPerfectLowerCaseSentenceIsAPangram() {
         assertTrue(ex005.isPangram("abcdefghijklmnopqrstuvwxyz"));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkPerfectUpperCaseSentenceIsAPangram() {
         assertTrue(ex005.isPangram("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkSentenceWithLowerCaseIsAPangram() {
         assertTrue(ex005.isPangram("the quick brown fox jumps over the lazy dog"));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
+    // Test to check true for Pangram with all capital letters
+    @Test
+    public void checkSentenceWithUpperCaseIsAPangram() {
+        assertTrue(ex005.isPangram("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"));
+    }
+
     @Test
     public void checkSentenceWithMixedUpperAndLowerCaseIsAPangram() {
         assertTrue(ex005.isPangram("The Five boXing wiZards Jump QuicklY"));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkSentenceWithMissingLetterXIsNotAPangram() {
         assertFalse(ex005.isPangram("the quick brown fo jumps over the lazy dog"));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkSentenceWithMissingLetterHIsNotAPangram() {
         assertFalse(ex005.isPangram("five boxing wizards jump quickly at it"));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkSentenceWithUnderscoresIsAPangram() {
         assertTrue(ex005.isPangram("the_quick_brown_fox_jumps_over_the_lazy_dog"));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkSentenceWithNumbersIsAPangram() {
         assertTrue(ex005.isPangram("the 1 quick brown fox jumps over the 2 lazy dogs"));
     }
 
+    // Test to check false for non Pangram sentence
+    @Test
+    public void checkSentenceIsNotPangram() {
+        assertFalse(ex005.isPangram("The tortoise and the hare decided to race"));
+    }
+
+    // Test to check true for multiple Pangrams
+    @Test
+    public void checkMultipleSentencesArePangram() {
+        assertTrue(ex005.isPangram("the quick brown fox jumps over the lazy dog and the quick onyx goblin jumps over the lazy dwarf"));
+    }
+
+    // Test to check false for all non alphabetic characters
+    @Test
+    public void checkSentenceWithNoAlphabetsIsNotPangram() {
+        assertFalse(ex005.isPangram("123_{]]}!@#$%^&*()?><+-789"));
+    }
+
+    // Test to check true for Pangram mixed with numbers and special characters
+    @Test
+    public void checkSentenceWithMixedCharIsPangram() {
+        assertTrue(ex005.isPangram("1.All (20) questions asked, by five watched experts amaze the judge!!!"));
+    }
+
+    // Test to check true for fully upper and lowercase Pangrams
+    @Test
+    public void checkSentenceWithUpperandLowerCasePangramsIsPangram() {
+        assertTrue(ex005.isPangram("Jim quickly realized that the beautiful gowns are expensive and WE PROMPTLY JUDGED ANTIQUE IVORY BUCKLES FOR THE NEXT PRIZE."));
+    }
+
+    // Test to check true for NullPointer Exception
+    @Test
+    public void checkNullPointerException() {
+        try {
+            ex005.isPangram(null);
+            fail("Failed to throw Exception");
+        } catch (NullPointerException expected) {
+            assertEquals(NullPointerException.class, expected.getClass());
+            System.out.println(expected);
+        }
+    }
 }
