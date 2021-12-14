b0a931aa81656061e70fc8f672af079c3a4ea2ac
HumaAltaf5 <huma.altaf@gmail.com>
2021-12-14 17:22:55 +0000

    Refactor code in Exercise003.java to reduce code repitition -HumaAltaf5

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -1,32 +1,24 @@
 package com.techreturners.exercise003;
 
+import java.util.Arrays;
+
 public class Exercise003 {
 
     int getIceCreamCode(String iceCreamFlavour) {
-        int code = 0;
         // check for null or empty and throw Exception
         if (iceCreamFlavour == null || iceCreamFlavour.isEmpty()) {
             throw new NullPointerException("Cannot compare null or empty word");
-        } else if (iceCreamFlavour.equals("Pistachio")) {
-            code = 0;
-        }  else if (iceCreamFlavour.equals("Raspberry Ripple")) {
-            code = 1;
-        } else if (iceCreamFlavour.equals("Vanilla")) {
-            code = 2;
-        } else if (iceCreamFlavour.equals("Mint Chocolate Chip")) {
-            code = 3;
-        } else if (iceCreamFlavour.equals("Chocolate")) {
-            code = 4;
-        } else if(iceCreamFlavour.equals("Mango Sorbet")) {
-            code = 5;
+        } else  {
+            return Arrays.asList(iceCreamFlavours()).indexOf(iceCreamFlavour);
         }
-        return code;
     }
 
     String[] iceCreamFlavours() {
         String[] icecream = { "Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
         return icecream;
     }
+
+
 
 }
 
